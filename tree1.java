7aa8e5bc0ca531fa45d14351695d4494759669a8
Bikrant-kumar <55061912+Bikrant-kumar@users.noreply.github.com>
2019-10-29 15:59:29 +0530

    Update tree1.java

--- tree1.java
+++ tree1.java
@@ -2,17 +2,14 @@
     static int N = 15; 
       
     // Function for finding maximum sum 
-    static int maxPathSum(int tri[][], int m, int n) 
+    static int maxSum(int tri[][], int m, int n) 
     { 
-        // loop for bottom-up calculation 
+     
         for (int i = m - 1; i >= 0; i--) 
         { 
             for (int j = 0; j <= i; j++) 
             { 
-                // for each element, check both 
-                // elements just below the number 
-                // and below right to the number 
-                // add the maximum of them to it 
+
                 if (tri[i + 1][j] > tri[i + 1][j + 1]) 
                     tri[i][j] += tri[i + 1][j]; 
                 else
@@ -20,7 +17,7 @@
             } 
         } 
       
-        // return the top element 
-        // which stores the maximum sum 
+        
+        //stores the maximum sum 
         return tri[0][0]; 
     }
